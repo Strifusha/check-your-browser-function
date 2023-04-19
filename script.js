d6588b170d9b68af2c480a2ea21ac1d84f6c1b9e
Strifusha <KatushaStrifusha@gmail.com>
2023-04-19 20:49:00 +0300

    different alerts added

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const getBtn = document.getElementById('browser-btn');
 
 function indicateBrowser() {
-alert(navigator.appCodeName);
+    const app = navigator.appVersion;
+    if (app.match('Safari/605')){
+        alert('You are using SAFARI browser');
+    }
+    if (app.match('OPR')){
+        alert('You are using OPERA browser');
+    }
+    if (app.match('Chrome') && !app.match('OPR')){
+        alert('You are using CHROME browser');
+    }
+  
+
+//console.log(app)
+
 }
 
 getBtn.addEventListener('click', indicateBrowser);
